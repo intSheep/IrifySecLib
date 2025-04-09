1538392f8d52276cdeb643d66a17a2a5203fa97c
intSheep <intSheep@qq.com>
2025-04-09 17:57:46 +0800

    add R

---
+++ src/main/java/org/example/common/util/R.java
@@ -0,0 +1,61 @@
+package org.example.common.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description 规范返回结果
+ * @author: zpy
+ * @Date: 2025/4/9
+ */
+public class R extends HashMap<String, Object> {
+
+    public R() {
+        put("code", "success");
+        put("msg", "success");
+    }
+
+    public static R error(Integer code ,String msg) {
+        R r = new R();
+        r.put("code", code);
+        r.put("msg", msg);
+        return r;
+    }
+
+    public static R error(){
+        return error(500,"服务器未知异常");
+    }
+
+    public static R ok(String msg){
+        R r = new R();
+        r.put("msg", msg);
+        return r;
+    }
+
+    public static R ok(String msg,String data){
+        R r = new R();
+        r.put("msg", msg);
+        r.put("data", data);
+        return r;
+    }
+
+    public static R ok(){
+       return new R();
+    }
+
+    public static R ok(Map<String,Object> data){
+        R r = new R();
+        r.putAll(data);
+        return r;
+    }
+
+    public R put(String key, Object value){
+        super.put(key, value);
+        return this;
+    }
+
+    public R setData(Object data){
+        this.put("data", data);
+        return this;
+    }
+}
--- src/main/java/org/example/moudels/rce/code/CodeController.java
+++ src/main/java/org/example/moudels/rce/code/CodeController.java
@@ -1,9 +1,12 @@
 package org.example.moudels.rce.code;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.example.common.util.R;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @Date: 2025/4/9 09:27
  */
 @Slf4j
-@Api(value="CodeController",tags = "RCE-代码注入")
+@Api(value="CodeController",tags = "RCE-远程命令执行")
 @Controller
 @CrossOrigin(origins = "*")
 @RequestMapping("/code")
@@ -22,4 +25,12 @@ public class CodeController {
     public String code() {
         return "vul/rce/code";
     }
+
+    @GetMapping("/vulGroovy")
+    @ResponseBody
+    @ApiOperation(value = "漏洞常见：RCE-Groovy",notes = "")
+    public R vulGroovy() {
+
+    }
+
 }
